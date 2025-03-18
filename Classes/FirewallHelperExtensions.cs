ab95058978621a07a3aa26781ac0a3e1bacd3391
Cedric Cullingworth <cedric@sbsdomain.com>
2025-03-18 13:04:02 +0200

    Remove an unused class

--- Classes/FirewallHelperExtensions.cs
+++ Classes/FirewallHelperExtensions.cs
@@ -1,25 +1,4 @@
-﻿using System;
-using System.Net;
-
-using WindowsFirewallHelper.Addresses;
-
-namespace BlocklistManager.Classes;
-
-/// <summary>
-///  Any extension methods or properties that are used to extend the functionality of the FirewallHelper library.
-/// </summary>
-public static class IPRangeExtensions
-{
-    public static IPRange FromSubnet( this IPRange owner, IPAddress subnet )
-    {
-        string[] parts = subnet.ToString( ).Split( '/' );
-        if ( parts.Length != 2 )
-            throw new ArgumentException( "Invalid subnet format." );
-        IPAddress startAddress = IPAddress.Parse( parts[ 0 ] );
-        IPAddress endAddress = IPAddress.Parse( parts[ 1 ] );
-        return new IPRange( startAddress, endAddress );
-    }
-}
+﻿namespace BlocklistManager.Classes;
 
 /// <summary>
 ///  Any extension methods or properties that are used to extend the functionality of the FirewallHelper library.
